a2eafc87c98024d172c5cf7d2a1618e55f318988
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-09-28 14:39:49 -0700

    user routes controllers and model created

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
